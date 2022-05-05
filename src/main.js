f521cf54a0cde517ad3874ab279687b2bd08b879
altannylmz <96581804+altannylmz@users.noreply.github.com>
2022-05-05 18:16:20 +0300

    font-awesome added to plugins

--- src/main.js
+++ src/main.js
@@ -14,12 +14,10 @@ const app = createApp(App);
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../src/assets/scss/main.scss';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faArrowDown, faPen, faPlay, faStop, faCircleMinus} from '@fortawesome/free-solid-svg-icons';
-library.add(faArrowDown, faPen, faPlay, faStop, faCircleMinus);
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 import notify from './plugins/notify';
+import FontAwesomeIcon from './plugins/font-awesome';
+
 app.use(notify);
 app.use(router);
 app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
---
+++ src/plugins/font-awesome.js
@@ -0,0 +1,5 @@
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faArrowDown, faPen, faPlay, faStop, faCircleMinus} from '@fortawesome/free-solid-svg-icons';
+library.add(faArrowDown, faPen, faPlay, faStop, faCircleMinus);
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+export default FontAwesomeIcon;
