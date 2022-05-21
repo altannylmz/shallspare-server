b63483887fb0acfddcfd656240f977c7da3fb33d
altannylmz <96581804+altannylmz@users.noreply.github.com>
2022-05-22 00:45:16 +0300

    new database area developed, few improvements developed

--- src/views/Database/NewDatabase.vue
+++ src/views/Database/NewDatabase.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="add-task-page content position-absolute start-0 end-0 px-4">
-    <form @submit.prevent="newRemoteMachine(databases.mysql)" class="bg-white shadow-sm p-4">
+    <form @submit.prevent="newDatabase" class="bg-white shadow-sm p-4">
       <p class="text-start title">
         New Database
       </p>
       <div class="form-group">
         <label for="database-type">Type</label>
-        <select v-model="databases.mysql.type" class="form-control" id="database-type" name="machineType" @change="changeRemoteMachineType($event)">
-          <option disabled="" selected="">Type</option>
+        <select v-model="type" class="form-control" id="database-type"
+                name="machineType" @change="type = $event.target.value">
+          <option value="-1" disabled="" selected="">Type</option>
           <option value="0" id="ftp">Mysql</option>
         </select>
       </div>
       <div class="form-group">
         <label for="tag">Tag</label>
-        <input v-model="databases.mysql.tag" type="text" class="form-control" id="tag">
+        <input v-model="mysql.tag" type="text" class="form-control" id="tag">
       </div>
       <div class="form-group">
         <label  for="hostname">Hostname</label>
-        <input v-model="databases.mysql.hostname" type="text" class="form-control" id="hostname">
+        <input v-model="mysql.hostname" type="text" class="form-control" id="hostname">
       </div>
       <div class="form-group">
         <label for="password">Password</label>
-        <input v-model="databases.mysql.password" type="text" class="form-control" id="password">
+        <input v-model="mysql.password" type="text" class="form-control" id="password">
       </div>
       <div class="form-group">
         <label for="database-name">Database Name</label>
-        <input v-model="databases.mysql.databaseName" type="text" class="form-control" id="database-name">
+        <input v-model="mysql.databaseName" type="text" class="form-control" id="database-name">
       </div>
       <div class="form-group">
         <label for="port">Port</label>
-        <input v-model="databases.mysql.port" type="text" class="form-control" id="port">
+        <input v-model="mysql.port" type="number" class="form-control" id="port">
       </div>
       <button type="submit" class="container-fluid text-center">Add</button>
     </form>
@@ -41,39 +42,58 @@ export default {
 	name: 'NewRemoteMachine',
 	data() {
 		return {
-			databases: {
-				mysql: {
-					type: '',
-					tag: '',
-					hostname: '',
-					username: '',
-					password: '',
-					databaseName: '',
-					port: '',
-				},
+			type: '-1',
+			mysql: {
+				tag: '',
+				hostname: '',
+				username: '',
+				password: '',
+				databaseName: '',
+				port: '',
 			},
 		};
 	},
 	methods: {
-		newRemoteMachine(machine) {
-			if (machine.username.length
-          || machine.hostname.length
-          || machine.password.length
-          || machine.port.length) {
-				console.log('test');
-			} else {
-				this.$notify.warning('Fill Empty Area');
-			}
-		},
-		changeRemoteMachineType() {
+		newDatabase() {
+			if (this.type !== '-1') {
+				switch (Number(this.type)) {
+					case 0:
+						if (this.mysql.tag.length
+                || this.mysql.hostname.length
+                || this.mysql.username.length
+                || this.mysql.password.length
+                || this.mysql.databaseName.length
+                || this.mysql.port !== '') {
+							this.$db.run('INSERT INTO Mysql(type,tag,host,user_name,password,database_name,port) values (?,?,?,?,?,?,?)', [
+								this.type,
+								this.mysql.tag,
+								this.mysql.hostname,
+								this.mysql.username,
+								this.mysql.password,
+								this.mysql.databaseName,
+								this.mysql.port,
+							], err => {
+								if (!err) {
+									this.$notify.success('Successful.');
+									this.mysql = {
+										tag: '',
+										hostname: '',
+										username: '',
+										password: '',
+										databaseName: '',
+										port: '',
+									};
+									this.type = '-1';
+								}
+							});
+						} else {
+							this.$notify.warning('Fill Empty Area');
+						}
 
-		},
-		testConnection(machine) {
-			if (machine.username.length
-          || machine.hostname.length
-          || machine.password.length
-          || machine.port.length) {
-				console.log('test');
+						break;
+					default:
+						break;
+				}
 			} else {
 				this.$notify.warning('Fill Empty Area');
 			}
--- src/views/RemoteMachine/NewRemoteMachine.vue
+++ src/views/RemoteMachine/NewRemoteMachine.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="add-task-page content position-absolute start-0 end-0 px-4">
-    <form @submit.prevent="newRemoteMachine(remoteMachine.ftp)" class="bg-white shadow-sm p-4">
+    <form @submit.prevent="newRemoteMachine" class="bg-white shadow-sm p-4">
       <p class="text-start title">
         New Remote Machine
       </p>
       <div class="form-group">
         <label for="machine-type">Type</label>
-        <select v-model="remoteMachine.ftp.type" @change="remoteMachine.ftp.type = $event.target.value"
+        <select v-model="type" @change="type = $event.target.value"
                 class="form-control" id="machine-type" name="machineType">
           <option value="-1" disabled="" selected="">Type</option>
           <option value="0" id="ftp">FTP</option>
@@ -14,21 +14,21 @@
       </div>
       <div class="form-group">
         <label for="hostname">Hostname</label>
-        <input v-model="remoteMachine.ftp.hostname" type="text" class="form-control" id="hostname">
+        <input v-model="ftp.hostname" type="text" class="form-control" id="hostname">
       </div>
       <div class="form-group">
         <label  for="username">Username</label>
-        <input v-model="remoteMachine.ftp.username" type="text" class="form-control" id="username">
+        <input v-model="ftp.username" type="text" class="form-control" id="username">
       </div>
       <div class="form-group">
         <label for="password">Password</label>
-        <input v-model="remoteMachine.ftp.password" type="text" class="form-control" id="password">
+        <input v-model="ftp.password" type="text" class="form-control" id="password">
       </div>
       <div class="form-group">
         <label for="port">Port</label>
-        <input v-model="remoteMachine.ftp.port" type="number" class="form-control" id="port" placeholder="21">
+        <input v-model="ftp.port" type="number" class="form-control" id="port" placeholder="21">
       </div>
-      <button @click.prevent="testConnection(remoteMachine.ftp)"
+      <button @click.prevent="testConnection"
               class="min-btn mt-2">Test Connection</button>
       <button type="submit" class="container-fluid text-center">Add</button>
     </form>
@@ -41,56 +41,66 @@ export default {
 	name: 'NewRemoteMachine',
 	data() {
 		return {
-			remoteMachine: {
-				ftp: {
-					type: '-1',
-					username: '',
-					hostname: '',
-					password: '',
-					port: '',
-				},
+			type: '-1',
+			ftp: {
+				username: '',
+				hostname: '',
+				password: '',
+				port: '',
 			},
 		};
 	},
 	methods: {
-		newRemoteMachine(machine) {
-			if (machine.type !== '-1'
-          && machine.username.length
-          && machine.hostname.length
-          && machine.password.length
-          && machine.port) {
-				this.$db.run('INSERT INTO Remote(type,host,user_name,password,port) values (?,?,?,?,?)', [
-					machine.type,
-					machine.username,
-					machine.hostname,
-					machine.password,
-					machine.port,
-				], err => {
-					if (!err) {
-						this.$notify.success('Successful.');
-						this.remoteMachine.ftp = {
-							type: '-1',
-							username: '',
-							hostname: '',
-							password: '',
-							port: '',
-						};
+		newRemoteMachine() {
+			if (this.type !== '-1') {
+				if (this.ftp.username.length
+            && this.ftp.hostname.length
+            && this.ftp.password.length
+            && this.ftp.port) {
+					switch (Number(this.type)) {
+						case 0:
+							this.$db.run('INSERT INTO Remote(type,host,user_name,password,port) values (?,?,?,?,?)', [
+								this.ftp.type,
+								this.ftp.username,
+								this.ftp.hostname,
+								this.ftp.password,
+								this.ftp.port,
+							], err => {
+								if (!err) {
+									this.$notify.success('Successful.');
+									this.ftp = {
+										type: '-1',
+										username: '',
+										hostname: '',
+										password: '',
+										port: '',
+									};
+								}
+							});
+							break;
+						default:
+							break;
 					}
-				});
+				} else {
+					this.$notify.warning('Fill Empty Area');
+				}
 			} else {
 				this.$notify.warning('Fill Empty Area');
 			}
 		},
-		async testConnection(machine) {
-			if (machine.type !== '-1'
-          && machine.username.length
-          && machine.hostname.length
-          && machine.password.length
-          && machine.port) {
-				if (await Helper.ftpTest(machine.hostname, machine.username, machine.password, machine.port)) {
-					this.$notify.success('Connection successful.');
+		async testConnection() {
+			if (this.type !== '-1') {
+				if (this.ftp.username.length
+            && this.ftp.hostname.length
+            && this.ftp.password.length
+            && this.ftp.port) {
+					if (await Helper.ftpTest(this.ftp.hostname, this.ftp.username, this.ftp.password, this.ftp.port)) {
+						this.$notify.success('Connection successful.');
+					} else {
+						this.$notify.failure('Connection failed.');
+					}
 				} else {
-					this.$notify.failure('Connection failed.');
+					this.$notify.warning('Fill Empty Area');
 				}
 			} else {
 				this.$notify.warning('Fill Empty Area');
